1556b3c8482ae9875d45796abe3f00676c1b5319
Christian Brunotte <cbrunotte@netcologne.de>
2019-09-20 12:07:33 +0200

    scripts

---
+++ home/30-lathspell-xmodmap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo $0
+
+echo "keycode 134 = XF86Launch1     " | xmodmap -   # "@" unten rechts
+
+echo "clear lock                    " | xmodmap -   # "/" auf CapsLock
+echo "keycode 66 = slash            " | xmodmap -   # "/" auf CapsLock
